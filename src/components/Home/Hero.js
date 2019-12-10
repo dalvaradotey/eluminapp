4db33a7e63c23c34b8f58a9dbeefc6288427d5da
Diego Alvarado Otey <diego@Diegos-MacBook-Pro.local>
2019-12-09 21:49:07 -0300

    UPDATE

--- src/components/Home/Hero.js
+++ src/components/Home/Hero.js
@@ -15,8 +15,18 @@ const Hero = () => (
       <h1 className="f1 logo-font normal mt0">eluminapp</h1>
     </div>
     <div className="mt2">
-      <p className="f3 ma0 mt2">¿Quieres enseñar algo?</p>
-      <p className="f4 ma0 mt1">o quizás <span className="b">¿Te gustaría aprender algo?</span></p>
+      <h2 className="fw5 mb2" style={{ marginLeft: '-100px' }}>¿Te gustaría enseñar
+        <div class="slidingVertical b">
+          <span>Baile?</span>
+          <span>Cocina?</span>
+          <span>Política?</span>
+          <span>Fotografía?</span>
+          <span>Idiomas?</span>
+        </div>
+      </h2>
+      <p className="f4 ma0 mt1">o quizás <span className="b">¿Quieres aprender?</span></p>
+    </div>
+    <div className="mt0">
     </div>
   </div>
 );
--- src/components/Suscribe/Modal.js
+++ src/components/Suscribe/Modal.js
@@ -1,7 +1,57 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import { ModalRoutingContext } from 'gatsby-plugin-modal-routing'
+import React, { Component } from 'react';
+import { Link } from 'gatsby';
+import swal from '@sweetalert/with-react';
+import { ModalRoutingContext } from 'gatsby-plugin-modal-routing';
+import Airtable from 'airtable';
 
+const base = new Airtable({
+  apiKey: process.env.AIRTABLE_API_KEY
+}).base('appPcTl1DJ253jMIF')
+
+class Modal extends Component {
+  state = {
+    email: '',
+  };
+
+  onClick = () => {
+    base('Table 1').create({
+      "Email": this.state.email
+    }, (err, record) => {
+      if (err) {
+        return;
+      }
+      swal('Registrado.', { icon: 'success'});
+    })
+  }
+
+  render = () => (
+    <ModalRoutingContext.Consumer>
+      {({ modal, closeTo }) => (
+        <div>
+          {modal ? (
+            <Link to={closeTo}>
+              Close
+            </Link>
+          ) : (
+            <header>
+              <h1>
+                Website Title
+              </h1>
+            </header>
+          )}
+
+          <h2>Modal Page</h2>
+          <input type="text" onChange={e => this.setState({ email: e.target.value })} value={this.state.email}/>
+          <button onClick={this.onClick}>Enviar</button>
+          <Link to="/">Go back to the homepage</Link>
+        </div>
+      )}
+    </ModalRoutingContext.Consumer>    
+  );
+}
+
+export default Modal;
+/*
 const Modal = () => (
   <ModalRoutingContext.Consumer>
     {({ modal, closeTo }) => (
@@ -27,3 +77,4 @@ const Modal = () => (
 )
 
 export default Modal;
+*/
